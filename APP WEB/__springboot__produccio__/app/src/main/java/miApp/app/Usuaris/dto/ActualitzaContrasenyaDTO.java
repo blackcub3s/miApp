ee05956aa6ab2f253e09ef5776d61972f3dfbef3
blackcub3s <santiago.sanchez.sans.44@gmail.com>
2025-03-22 12:50:24 +0100

    tots els missatges d'error passats a utils ValidacionsUsuari

--- APP WEB/__springboot__produccio__/app/src/main/java/miApp/app/Usuaris/dto/ActualitzaContrasenyaDTO.java
+++ APP WEB/__springboot__produccio__/app/src/main/java/miApp/app/Usuaris/dto/ActualitzaContrasenyaDTO.java
@@ -7,8 +7,8 @@ import lombok.Getter;
 import lombok.Setter;
 
 //IMPORTO LES VALIDACIONS
-import static miApp.app.utils.ValidacionsUsuari.MISSATGE_ERROR_CONTRASENYA;
-import static miApp.app.utils.ValidacionsUsuari.REGEX_CONTRASENYA;
+import static miApp.app.utils.ValidacionsUsuari.*;
+
 
 // UTILITZEM AQUESTA CLASSE PER VALIDAR L'ENTRADA DE DADES A L'ENDPOINT D'UNA CREACIÓ DE CONTRASENYA
 // AMB PATCH (e.g. localhost:8080/api/usuaris/{id}/contrasenya).
@@ -19,8 +19,8 @@ import static miApp.app.utils.ValidacionsUsuari.REGEX_CONTRASENYA;
 public class ActualitzaContrasenyaDTO {
 
     //LES ANOTACIONS CONTENEN LES VALIDACIONS (QUE POSEM EN utils, validaciosnUsuari)!!!
-    @Size(min = 8, max = 25, message = "¡La contraseña debe tener entre 8 y 25 caracteres!") //també va en tàndem a @valid (COMPTE QUE NO CUBREIX EL CAS EN QUE ENTRI null EN COMPTES DE STRING BUIT)
-    @NotBlank(message = "¡La contraseña no puede estar vacía, ni ser null! Usa como clave contrasenya en la clave del JSON!")     //@NotBlank VA EN TÀNDEM A @valid dins del paràmetre del controlador de la funció actualitzaContrasenya
+    @Size(min = TAMANY_MINIM_CONTRASENYA, max = TAMANY_MAXIM_CONTRASENYA, message = MISSATGE_ERROR_TAMANY) //també va en tàndem a @valid (COMPTE QUE NO CUBREIX EL CAS EN QUE ENTRI null EN COMPTES DE STRING BUIT)
+    @NotBlank(message = MISSATGE_NOT_BLANK_GENERIC)     //@NotBlank VA EN TÀNDEM A @valid dins del paràmetre del controlador de la funció actualitzaContrasenya
     @Pattern(regexp = REGEX_CONTRASENYA, message = MISSATGE_ERROR_CONTRASENYA)
     private String contrasenya; //COMPTE, QUE A LA BASE DE DADES EL CAMP ES DIU hashContrasneya, perquè guardem el hash. Però en el DTO diem contrasenya, ja que no està hashejada encara.
 }
--- APP WEB/__springboot__produccio__/app/src/main/java/miApp/app/utils/ValidacionsUsuari.java
+++ APP WEB/__springboot__produccio__/app/src/main/java/miApp/app/utils/ValidacionsUsuari.java
@@ -3,7 +3,19 @@ package miApp.app.utils;
 //VALIDO ELS CAMPS DE LA TAULA USUARI
 public class ValidacionsUsuari {
 
-    //validació camp contrasenya (TAMBÉ CAL POSAR el not null i el size min=8 i max=25, no te n'oblidis),
+    //validació camp contrasenya (TAMBÉ CAL POSAR el @not null i el size min=8 i max=25, no te n'oblidis),
     public static final String REGEX_CONTRASENYA =  "^(?=[a-zA-Z0-9àéèíòóú_.ÀÉÈÍÒÓÚñÑ]+$)(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?!.*\\s).{8,25}$";
     public static final String MISSATGE_ERROR_CONTRASENYA = "¡La contraseña debe contener al menos una minúscula, una mayúscula, un número y no puede contener espacios! Se admiten puntos y barras bajas pero no otros caracteres";
+
+    //validacio contrasenya (@Size)
+    public static final int TAMANY_MINIM_CONTRASENYA = 8;
+    public static final int TAMANY_MAXIM_CONTRASENYA = 25;
+    public static final String MISSATGE_ERROR_TAMANY = "¡La contraseña debe tener entre 8 y 25 caracteres!";
+
+
+    //MISSATGE GENÈRIC ASSOCIAT A ANOTACIÓ @NOT_BLANK PER LES VALIDACIONS D'USUARI QUE FACIS
+    public static final String MISSATGE_NOT_BLANK_GENERIC = "¡El campo no puede estar vacío, ni ser null! Comprueba que hayas usado la clave correcta en el JSON de entrada a la api!";
+
 }
+
+
