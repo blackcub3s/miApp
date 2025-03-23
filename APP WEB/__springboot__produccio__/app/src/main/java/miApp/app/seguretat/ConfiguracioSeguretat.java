d4606e4477649b60fb35659569eae9f5e7103b75
blackcub3s <santiago.sanchez.sans.44@gmail.com>
2025-03-23 02:10:17 +0100

    afegida una security filter chain per a bloquejar certs endpoints. Continua ara amb el hash i despres afegir el JWT.

---
+++ APP WEB/__springboot__produccio__/app/src/main/java/miApp/app/seguretat/ConfiguracioSeguretat.java
@@ -0,0 +1,28 @@
+package miApp.app.seguretat;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+//AQUESTA CLASSE PERMET PROTEGIR O DESPROTEGIR ELS ENDPOINTS. ES NECESSARIA PERQUÈ ELS ENDPOINTS, EN AFEGIR
+//LA DEPENDÈNCIA spring-boot-starter-security QUEDEN PROTEGITS I QUALSEVOL CRIDA A ELLS DÓNA ERROR 401
+@Configuration
+@EnableWebSecurity
+public class ConfiguracioSeguretat {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        //.requestMatchers("/api/usuaris").authenticated()  //ENDPOINT PROTEGIT
+                        .requestMatchers("/api/nreUsuaris").authenticated() //ENDPOINT PROTEGIT
+                        .requestMatchers("/api/**").permitAll()  // PERMET QUE LA RESTA D'ENDPOINTS dins /api/ SIGUIN PUBLICS
+                )
+                .csrf(csrf -> csrf.disable());
+
+        return http.build();
+    }
+}
