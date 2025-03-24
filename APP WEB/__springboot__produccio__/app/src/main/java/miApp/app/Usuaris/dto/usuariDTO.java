2cdf3d331820a61834fd363a60117aabda882c78
blackcub3s <santiago.sanchez.sans.44@gmail.com>
2025-03-24 16:51:22 +0100

    asd

--- APP WEB/__springboot__produccio__/app/src/main/java/miApp/app/Usuaris/dto/usuariDTO.java
+++ APP WEB/__springboot__produccio__/app/src/main/java/miApp/app/Usuaris/dto/usuariDTO.java
@@ -3,6 +3,7 @@ package miApp.app.Usuaris.dto;
 import miApp.app.utils.validacio.Contrasenya;
 import miApp.app.utils.validacio.CorreuElectronic;
 
+//L'USAREM PER A LA SOLICITUD POST (de modificacio completa de l'usauri) y PUT (de actualitzacio del recurs compplet de l'usuari)
 public class usuariDTO {
 
     @CorreuElectronic
